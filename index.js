0434cc4146dade40da9d6fba1f5c1c5efecce3af
Owen <owen@arborbridge.com>
2019-08-07 17:29:11 +0200

    529

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ const initialMasterFunction = () => {
   return "Overwriting on branched content"
   return "Overwriting in main branch"
 }
+
+const f = () => {
+  //change made to master at 5:28pm
+  return
+}
