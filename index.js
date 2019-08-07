a07c59f5f184fe20b3ccc7526d3b2f4c9b20e09b
Owen <owen@arborbridge.com>
2019-08-07 17:32:09 +0200

    532

--- index.js
+++ index.js
@@ -5,5 +5,10 @@ const initialMasterFunction = () => {
 
 const f = () => {
   //change made to master at 5:28pm
+  console.log('this is master change at 5:32pm')
   return
+}
+
+const also = () => {
+  console.log('also changed master 5:32pm')
 }
